48dffea1696dfeb616b9396eea6ffc5de4c8bde8
Alex Taylor <ataylor@skylabhq.com>
2015-08-13 00:28:00 +0000

    Adds quick integration test for Timesheet CSV import

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -1,2 +1,8 @@
 class Timesheet < ActiveRecord::Base
+  def self.import_from_csv(file)
+    CSV.foreach(file.path, headers:true) do |row|
+      # you can use row["Service Group"] here to pull out the row
+      #  regardless of its position. Cool!
+    end
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,6 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
+  include ActionDispatch::TestProcess # required for fixture_file_upload to work
   # Add more helper methods to be used by all tests here...
 end
